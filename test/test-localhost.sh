40ae520f918520cd041627f00d9a8e4ef9762b3c
Alisson Sol <git@AlissonSol.com>
2026-04-10 17:18:10 -0400

    Apple virtualization keystroke workaround

---
+++ test/test-localhost.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Test that localhost HTTP returns status 200.
+# Avoids shifted characters (|, :, %, {, }) that are unreliable
+# when typed via CGEvent on macOS UTM with Apple Virtualization.
+curl -k -sI http://localhost | head -n 1 | cut -d ' ' -f 2

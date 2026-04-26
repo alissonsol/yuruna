0c9c87d69be7347e36aa582c4302363c33fbcc31
Alisson Sol <git@AlissonSol.com>
2026-04-26 23:58:34 +0100

    More stable site test

--- test/test-localhost.sh
+++ test/test-localhost.sh
@@ -2,4 +2,4 @@
 # Test that localhost HTTP returns status 200.
 # Avoids shifted characters (|, :, %, {, }) that are unreliable
 # when typed via CGEvent on macOS UTM with Apple Virtualization.
-curl -k -sI http://localhost | head -n 1 | cut -d ' ' -f 2
+curl -k -sI http://localhost | head -n 1
